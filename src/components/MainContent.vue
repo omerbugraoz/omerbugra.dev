8c42adb53513880eb2e6f8ef1953d38e7cfdb3f8
OmerBugraOz <oomerbugraoz@gmail.com>
2025-06-11 13:23:26 +0300

    chore: enhance responsive

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="flex flex-col items-center py-30 w-full h-screen bg-[#FFFDF6]">
+  <section class="flex flex-col items-center justify-center md:justify-normal md:py-30 w-full h-screen bg-[#FFFDF6]">
     <img
       :src="photoUrl"
       class="rounded-full w-[240px] h-[240px] border-8 border-[#F5ECD5] object-cover"
       alt="Profile Picture"
     />
-    <h1 class="text-4xl font-bold mt-4">
+    <h1 class="text-4xl font-bold mt-15 md:mt-4">
       Hello, I'm
       <span class="relative">
         Ömer Buğra! <span class="absolute bottom-0 left-0 w-full h-[4px] bg-[#FFD70080] transform -rotate-2"></span>
@@ -26,12 +26,12 @@
     <SpeedDial
       mask
       type="semi-circle"
-      direction="down"
+      :direction="isMobile ? 'up' : 'down'"
       :model="items"
       :radius="120"
-      style="position: absolute; bottom: 35%"
+      :style="isMobile ? 'position: absolute; bottom: 5%' : 'position: absolute; bottom: 35%'"
       showIcon="pi pi-link"
-      hideIcon="pi pi-times"
+      hideIcon="pi pi-wave-pulse"
       @show="isSpeedDialOpen = true"
       @hide="isSpeedDialOpen = false"
       :buttonProps="{
@@ -54,6 +54,9 @@
 <script setup>
 import SpeedDial from 'primevue/speeddial'
 import { ref, watch } from 'vue'
+import { useDevice } from '../composables/useDevice'
+
+const { isMobile } = useDevice()
 
 const photoUrl = new URL('../assets/pp.jpeg', import.meta.url).href
 
